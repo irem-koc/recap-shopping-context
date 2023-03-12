47657e9902d15bddae7fd7f867ca0fb499d99f19
irem-koc <irem_koc_75@hotmail.com>
2023-03-12 13:20:32 +0300

    signup-added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import './Signup.css'
+import React from "react";
+import "./Signup.css";
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+    return (
+        <div className="signup">
+            <div className="input-name">
+                <label htmlFor="name">Name:</label>
+                <input
+                    className="name-input"
+                    type="text"
+                    name="name"
+                    autoFocus
+                    autoComplete="off"
+                />
+            </div>
+            <div className="input-surname">
+                <label htmlFor="name">Surname:</label>
+                <input
+                    className="surname-input"
+                    type="text"
+                    name="surname"
+                    autoComplete="off"
+                />
+            </div>
+            <button className="submit-btn">Submit</button>
+        </div>
+    );
+};
 
-export default Signup
+export default Signup;
